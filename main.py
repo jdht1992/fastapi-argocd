2b1c063a23c84a21e6b2b6ef7d24bf98211ecfca
diego herrera <jdht1992@gmail.com>
2026-06-18 16:45:51 -0600

    change de message test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "¡Hola desde Kubernetes!", "status": "Ready"}
+    return {"message": "¡Hola desde Kubernetes cambiemos el mensaje!", "status": "Ready"}
 
 
 @app.get("/health")
